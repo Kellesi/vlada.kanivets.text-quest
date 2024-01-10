4edc68d3b20c7e94abf2cdfbde1e00e81ba18a66
Vladyslava Kanivets <kanivetsvlada21@gmail.com>
2024-01-10 22:45:01 +0200

    stage and StageContextReaderJson + jsp/json/img files added

---
+++ src/main/java/ua/javarush/textquest/controller/StageController.java
@@ -0,0 +1,52 @@
+package ua.javarush.textquest.controller;
+
+import ua.javarush.textquest.dispatcher.MethodType;
+import ua.javarush.textquest.dispatcher.RequestMapping;
+import ua.javarush.textquest.entity.Stage;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class StageController {
+    private final Map<Integer, Stage> stages;
+
+    public StageController(Map<Integer, Stage> stages) {
+        this.stages = stages;
+    }
+
+    @RequestMapping(url = "/stage", method = MethodType.GET)
+    public void getStage(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            String choice = req.getParameter("choice");
+            int stageID = 0;
+            if (choice != null) {
+                stageID = Integer.parseInt(choice);
+            }
+
+            Stage stage = stages.get(stageID);
+            String description = stage.getStageDescription();
+            req.setAttribute("description", description);
+
+            Object[] choises = stage.getStepChoices();
+            req.setAttribute("choises", choises);
+
+            String stageImg = stage.getStageImg();
+            req.setAttribute("stageImg", stageImg);
+
+            if (stage.isDeadPoint()) {
+                req.getRequestDispatcher("/dead.jsp").forward(req, resp);
+            }
+            if (stage.isEndPoint()) {
+                req.getRequestDispatcher("/end.jsp").forward(req, resp);
+            }
+            req.getRequestDispatcher("/stage.jsp").forward(req, resp);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
